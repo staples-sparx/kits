ff5bade37f00574576d29e443490bdb7e15cc3ac
Alex Baranosky <alex@runa.com>
2012-09-06 10:34:59 -0700

    added lein multi

--- project.clj
+++ project.clj
@@ -1,7 +1,19 @@
+(def common-deps '[]) ;; if any ever exist
+
 (defproject com.runa/kits "1.0.0"
   :description "Runa base libraries"
   :plugins [[s3-wagon-private "1.1.2"]]
   :repositories {"releases" {:url "s3p://runa-maven/releases/"}
                  "snapshots" {:url "s3p://runa-maven/snapshots/"}}
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies ~(cons '[org.clojure/clojure "1.3.0"]
+                         common-deps)
+  :dev-dependencies [[jonase/kibit "0.0.3"]
+                     [jonase/eastwood "0.0.2"]
+                     [lein-multi "1.1.0"]]
+  :multi-deps {"1.2.0" [[org.clojure/clojure "1.2.0"]]
+               "1.2.1" [[org.clojure/clojure "1.2.1"]]
+               "1.3.0" [[org.clojure/clojure "1.3.0"]]
+               "1.4.0" [[org.clojure/clojure "1.4.0"]]
+               "1.5.0" [[org.clojure/clojure "1.5.0-alpha3"]]
+               :all ~common-deps}
   :aot [kits.foundation kits.queues kits.runtime])
