d1a558b8a4d00566abfdcc421c2870201e8dcb7c
Alex Baranosky <alex@runa.com>
2012-09-07 02:14:23 -0700

    added timestamp.clj from Furtive

---
+++ src/kits/timestamp.clj
@@ -0,0 +1,182 @@
+(ns kits.timestamp
+  (:import
+   java.text.SimpleDateFormat
+   [java.util Date Calendar TimeZone GregorianCalendar]))
+
+(set! *warn-on-reflection* true)
+
+;;; formats
+
+(def yyyy-mm-dd "yyyy-MM-dd")
+
+(defn yyyy-mm-dd? [x]
+  (boolean
+    (and (string? x)
+      (re-matches #"\d{4}-\d{2}-\d{2}" x))))
+
+(def yyyy-mm-dd-hh-mm "yyyy-MM-dd HH:mm")
+
+(defn yyyy-mm-dd-hh-mm? [x]
+  (boolean
+    (and (string? x)
+      (re-matches #"\d{4}-\d{2}-\d{2} \d{2}:\d{2}" x))))
+
+(def yyyy-mm-dd-hh-mm-ss "yyyy-MM-dd HH:mm:ss")
+
+(defn yyyy-mm-dd-hh-mm-ss? [x]
+  (boolean
+    (and (string? x)
+      (re-matches #"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}" x))))
+
+;;;
+
+(defn now []
+  (System/currentTimeMillis))
+
+(defn ^SimpleDateFormat simple-date-format
+  ([format-string]
+     (simple-date-format format-string "UTC"))
+  ([format-string tz-string]
+     (doto (SimpleDateFormat. format-string)
+       (.setTimeZone (TimeZone/getTimeZone tz-string)))))
+
+(defn ->timestamp [x]
+  (cond
+    (integer? x)
+    x
+
+    (yyyy-mm-dd? x)
+    (-> (simple-date-format yyyy-mm-dd) (.parse x) .getTime)
+
+    (yyyy-mm-dd-hh-mm? x)
+    (-> (simple-date-format yyyy-mm-dd-hh-mm) (.parse x) .getTime)
+
+    (yyyy-mm-dd-hh-mm-ss? x)
+    (-> (simple-date-format yyyy-mm-dd-hh-mm-ss) (.parse x) .getTime)
+
+    (and (string? x) (re-matches #"\d*" x))
+    (Long/parseLong x)
+
+    :else
+    (throw (IllegalArgumentException. (str "Don't know how to parse " (pr-str x))))))
+
+(defn ->str
+  ([x]
+     (->str x yyyy-mm-dd-hh-mm-ss))
+  ([x date-format]
+     (.format
+      (simple-date-format date-format)
+      (Date. (long (->timestamp x))))))
+
+(defn ->date-str [x]
+  (.format
+   (simple-date-format yyyy-mm-dd)
+   (Date. (long (->timestamp x)))))
+
+;;;
+
+(def ordered-units
+  [:milli
+   :second
+   :minute
+   :hour
+   :day
+   :month
+   :year])
+
+(def unit->calendar-unit
+  {:milli  {:type Calendar/MILLISECOND  :starts-at 0}
+   :second {:type Calendar/SECOND       :starts-at 0}
+   :minute {:type Calendar/MINUTE       :starts-at 0}
+   :hour   {:type Calendar/HOUR_OF_DAY  :starts-at 0}
+   :day    {:type Calendar/DAY_OF_MONTH :starts-at 1}
+   :month  {:type Calendar/MONTH        :starts-at 0}
+   :week   {:type Calendar/WEEK_OF_YEAR :starts-at 1}
+   :year   {:type Calendar/YEAR         :starts-at 0}})
+
+(defn assert-valid-unit [unit]
+  (when-not (contains? unit->calendar-unit unit)
+    (throw (IllegalArgumentException. (str unit " not one of " ordered-units)))))
+
+(defn truncate [timestamp unit]
+  (assert-valid-unit unit)
+  (let [cal (doto ^Calendar (GregorianCalendar. ^TimeZone (TimeZone/getTimeZone "UTC"))
+              (.setTime (Date. (long timestamp))))
+        units-to-drop (take-while #(not= unit %) ordered-units)]
+    (doseq [{:keys [type starts-at]} (map unit->calendar-unit units-to-drop)]
+      (.set cal type starts-at))
+    (.getTimeInMillis cal)))
+
+(defn add [timestamp unit val]
+  (assert-valid-unit unit)
+  (let [cal (doto ^Calendar (GregorianCalendar. ^TimeZone (TimeZone/getTimeZone "UTC"))
+              (.setTime (Date. (long timestamp)))
+              (.add (:type (unit->calendar-unit unit)) val))]
+    (-> cal .getTime .getTime)))
+
+(defn subtract [timestamp unit val]
+  (add timestamp unit (* -1 val)))
+
+(defn increment [timestamp unit]
+  (add timestamp unit 1))
+
+(defn decrement [timestamp unit]
+  (add timestamp unit -1))
+
+;;;
+
+(defn overlap? [[a0 a1] [b0 b1]]
+  (and
+    (< a0 b1)
+    (> a1 b0)))
+
+(defn ->timestamp-at-day-start [t]
+  (-> t ->timestamp (truncate :day)))
+
+(defn timestamp-at-day-end [ts]
+  (-> ts (add :day 1) (decrement :second)))
+
+(defn ->timestamp-at-day-end [t]
+  (-> t ->timestamp timestamp-at-day-end))
+
+(defmulti timestamp-ranges
+  (fn [_ _ interval]
+    (if (#{:minute :hour :day :week :year} (keyword interval))
+      :standard-interval
+      (keyword interval))))
+
+(defmethod timestamp-ranges :standard-interval [start-date end-date interval]
+  (let [interval (keyword interval)
+        start (->timestamp start-date)
+        end-ts (->timestamp end-date)
+        end (increment end-ts interval)
+        stdrd-decrementor (fn [[from to]]
+                            [from (decrement to :second)])
+        ranges (->> start
+                    (iterate #(increment % interval))
+                    (take-while #(<= % end))
+                    (partition 2 1))]
+    (map stdrd-decrementor ranges)) )
+
+(defmethod timestamp-ranges :month [start-date end-date interval]
+  (let [interval (keyword interval)
+        start-ts (->timestamp start-date)
+        end-ts (->timestamp end-date)
+        month-decrementor (fn [[from to]]
+                            [from (-> to
+                                      (truncate :month)
+                                      (decrement :second))])
+        ranges (->> (truncate start-ts :month)
+                    (iterate #(increment % interval))
+                    (take-while #(<= % (increment end-ts interval)))
+                    (partition 2 1))
+        first-month-pair (first ranges)
+        last-month-pair (last ranges)
+        month-pairs-middle (butlast (rest ranges))
+        fix-first (fn [[f _]] [start-ts _])
+        fix-last  (fn [[_ l]] [_ (timestamp-at-day-end end-ts)])
+        mid-month-ranges (flatten (map month-decrementor month-pairs-middle))
+        flat-month-ranges (flatten [(fix-first (month-decrementor first-month-pair))
+                                    mid-month-ranges
+                                    (fix-last last-month-pair)])]
+    (partition 2 flat-month-ranges)))
---
+++ test/kits/test/timestamp.clj
@@ -0,0 +1,118 @@
+(ns kits.test.timestamp
+  (:use clojure.test
+        kits.timestamp))
+
+
+(deftest test->timestamp-at-day-end
+  (are [t expected] (= (->timestamp-at-day-end t) (->timestamp expected))
+       "2012-05-01"  "2012-05-01 23:59:59"
+       "2012-05-01 00:00" "2012-05-01 23:59:59"
+       "2012-05-01 00:00:00" "2012-05-01 23:59:59"
+       1335830400000 "2012-05-01 23:59:59"))
+
+(deftest test->timestamp-at-day-start
+  (are [t expected] (= (->timestamp-at-day-start t) (->timestamp expected))
+       "2012-05-01" "2012-05-01 00:00:00"
+       "2012-05-01 11:11" "2012-05-01 00:00:00"     
+       "2012-05-01 05:55:55" "2012-05-01 00:00:00"
+       1335873634000 "2012-05-01 00:00:00"))
+
+(deftest testing-truncate
+  (are [t interval expected] (= (->str (truncate (->timestamp t) interval)) expected)
+       "2012-05-03 22:22:22" :second "2012-05-03 22:22:22"
+       "2012-05-03 22:22:22" :minute "2012-05-03 22:22:00"
+       "2012-05-03 22:22:22" :hour "2012-05-03 22:00:00"
+       "2012-05-03 22:22:22" :day "2012-05-03 00:00:00"
+       "2012-05-03 22:22:22" :month "2012-05-01 00:00:00"
+       "2012-05-03 22:22:22" :year "2012-01-01 00:00:00"))
+
+(deftest testing-timestamp
+  (are [t expected] (= (->str (->timestamp t)) expected)
+       "2012-05-01" "2012-05-01 00:00:00"
+       "2012-05-01 12:12" "2012-05-01 12:12:00"
+       "2012-05-01 12:12:12" "2012-05-01 12:12:12"
+       "1335830400000" "2012-05-01 00:00:00"
+       1335830400000 "2012-05-01 00:00:00")
+  (are [t] (thrown-with-msg? IllegalArgumentException #"Don't know how to parse" (->timestamp t))
+       nil
+       "cat"
+       "1.5"
+       1.5
+       1M))
+
+(deftest test-timestamp-ranges
+  
+  (testing "Calendar units are (milli second minute hour day month week year)"
+    
+    (testing "There is no support for millisecond/second ranges."
+      (is (thrown? IllegalArgumentException
+                   (timestamp-ranges "2000-01-01 12:00:01" "2000-01-01 12:00:10" :milli)))
+      (is (thrown? IllegalArgumentException
+                   (timestamp-ranges "2000-01-01 12:00:01" "2000-01-01 12:00:10" :second))))
+    
+    
+    (testing "Function timestamp-ranges works only for: "
+
+      (testing "minute ranges"
+        (is (= (->> (timestamp-ranges "2000-01-01 12:01" "2000-01-01 12:03" :minute)
+                    (apply concat)
+                    (map ->str))
+               ["2000-01-01 12:01:00" "2000-01-01 12:01:59"
+                "2000-01-01 12:02:00" "2000-01-01 12:02:59"
+                "2000-01-01 12:03:00" "2000-01-01 12:03:59"])))
+      
+      (testing "hour ranges"
+        (is (= (->> (timestamp-ranges "2000-01-01 12:00" "2000-01-01 14:00" :hour)
+                    (apply concat)
+                    (map ->str))
+               ["2000-01-01 12:00:00" "2000-01-01 12:59:59"
+                "2000-01-01 13:00:00" "2000-01-01 13:59:59"
+                "2000-01-01 14:00:00" "2000-01-01 14:59:59"])))
+    
+      (testing "day ranges"
+        (is (= (->> (timestamp-ranges "2000-01-01" "2000-01-03" :day)
+                    (apply concat)
+                    (map ->str))
+               ["2000-01-01 00:00:00" "2000-01-01 23:59:59"
+                "2000-01-02 00:00:00" "2000-01-02 23:59:59"
+                "2000-01-03 00:00:00" "2000-01-03 23:59:59"])))
+    
+      (testing "week ranges"
+        (is (= (->> (timestamp-ranges "2000-01-01" "2000-01-24" :week)
+                    (apply concat)
+                    (map ->str))
+               ["2000-01-01 00:00:00" "2000-01-07 23:59:59"
+                "2000-01-08 00:00:00" "2000-01-14 23:59:59"
+                "2000-01-15 00:00:00" "2000-01-21 23:59:59"
+                "2000-01-22 00:00:00" "2000-01-28 23:59:59"])))
+
+      (testing "month ranges"
+        (is (= (->> (timestamp-ranges "2000-01-01" "2000-03-02" :month)
+                    (apply concat)
+                    (map ->str))
+               ["2000-01-01 00:00:00" "2000-01-31 23:59:59"
+                "2000-02-01 00:00:00" "2000-02-29 23:59:59"
+                "2000-03-01 00:00:00" "2000-03-02 23:59:59"])))
+
+      (testing "year ranges"
+        (is (= (->> (timestamp-ranges "2000-01-01" "2003-03-02" :year)
+                    (apply concat)
+                    (map ->str))
+               ["2000-01-01 00:00:00" "2000-12-31 23:59:59"
+                "2001-01-01 00:00:00" "2001-12-31 23:59:59"
+                "2002-01-01 00:00:00" "2002-12-31 23:59:59"
+                "2003-01-01 00:00:00" "2003-12-31 23:59:59"]))))))
+
+
+(deftest test-add
+  (is (= (add (->timestamp "2012-05-21") :day 4)
+         (->timestamp "2012-05-25"))))
+
+(deftest test-subtract
+  (is (= (subtract (->timestamp "2012-05-25") :day 4)
+         (->timestamp "2012-05-21"))))
+
+(deftest test-->str-with-optional-format-specfied
+  (is (= "2012/05/21"
+         (->str (->timestamp "2012-05-21") "yyyy/MM/dd"))))
+
