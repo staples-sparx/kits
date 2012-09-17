727b58b02b5d4941f8351478669cc487aeec7bf3
Alex Baranosky <alex@runa.com>
2012-09-17 00:14:47 -0700

    upped version that I somehow missed

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def common-deps '[[org.clojure/tools.logging "0.2.3"]])
 
-(defproject com.runa/kits "1.1.1"
+(defproject com.runa/kits "1.2.0"
   :description "Runa base libraries"
   :plugins [[s3-wagon-private "1.1.2"]]
   :repositories {"releases" {:url "s3p://runa-maven/releases/"}
