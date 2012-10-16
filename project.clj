029ec0678f83dbad23150c1250cd71ef17f450e8
Ram Krishnan <kriyative@gmail.com>
2012-10-16 14:44:07 -0700

    Revision 1.2.10.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def common-deps '[[org.clojure/tools.logging "0.2.3"]])
 
-(defproject com.runa/kits "1.2.9  "
+(defproject com.runa/kits "1.2.10"
   :description "Runa base libraries"
   :plugins [[s3-wagon-private "1.1.2"]
             [lein-swank "1.4.4"]]
