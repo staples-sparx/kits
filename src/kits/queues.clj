957ae935f450d23ac8b25f45583f3cab4a8cf7af
Ram Krishnan <kriyative@gmail.com>
2012-11-18 12:08:05 -0800

    Refactored queues, and added priority queue support.

--- src/kits/queues.clj
+++ src/kits/queues.clj
@@ -1,34 +1,53 @@
 (ns kits.queues
+  (:refer-clojure :exclude [get peek])
   (:import
-   java.util.concurrent.TimeUnit
-   java.util.concurrent.ArrayBlockingQueue))
+   [java.util.concurrent
+    TimeUnit BlockingQueue ArrayBlockingQueue PriorityBlockingQueue]))
 
-(defn create
+(defn make-basic-queue
   "Create a new queue that can hold at max 'capacity' items"
-  [capacity]
-  (ArrayBlockingQueue. (int capacity)))
+  [& [capacity]]
+  (let [capacity (or capacity 10)]
+    (ArrayBlockingQueue. (int capacity))))
 
-(defn add
+(def create make-basic-queue)           ; compatibility alias
+
+(defn make-priority-queue
+  "Create a new priority-queue with `comparator` and `capacity` items"
+  [comparator & [capacity]]
+  (let [capacity (or capacity 10)]
+    (PriorityBlockingQueue. capacity comparator)))
+
+(defn offer!
   "Add a new msg to the queue. Returns false if the msg could not be
    added because the queue is full, true otherwise."
-  [q msg]
-  (.offer ^ArrayBlockingQueue q msg))
+  [^BlockingQueue q msg]
+  (.offer q msg))
 
-(defn fetch
+(def add offer!)                        ; compatibility alias
+
+(defn poll!
   "Retrieves a message from the queue, waiting if necessary until an
    element becomes available."
-  [q timeout-in-ms]
-  (.poll ^ArrayBlockingQueue q timeout-in-ms TimeUnit/MILLISECONDS))
+  [^BlockingQueue q & [timeout-in-ms]]
+  (.poll q (or timeout-in-ms 0) TimeUnit/MILLISECONDS))
 
-(defn used [q]
-  (.size ^ArrayBlockingQueue q))
+(def fetch poll!)                       ; compatibility alias
 
-(defn free [q]
-  (.remainingCapacity ^ArrayBlockingQueue q))
+(defn peek
+  "Retrieves, but does not remove, a message from the queue"
+  [^BlockingQueue q]
+  (.peek q))
+
+(defn used [^BlockingQueue q]
+  (.size q))
+
+(defn free [^BlockingQueue q]
+  (.remainingCapacity q))
 
 (defn stats
   "Return current stats for the queue"
-  [^ArrayBlockingQueue q]
+  [^BlockingQueue q]
   (let [s (used q)
         r (free q)]
     {:total (+ s r)
@@ -42,8 +61,8 @@
    when invoking 'start-thread-pool'"
   [thread-count name-prefix f & args]
   (doall
-   (map #(let [name (str name-prefix %)
-               t (Thread. ^Runnable (partial f name args) ^String name)]
+   (map #(let [^String name (str name-prefix %)
+               t (Thread. ^Runnable (partial f name args) name)]
            (.start t)
            t)
         (range 0 (int thread-count)))))
--- test/kits/test/queues.clj
+++ test/kits/test/queues.clj
@@ -2,16 +2,26 @@
   (:use
    clojure.test)
   (:require
+   [kits.homeless :as kits]
    [kits.queues :as q]))
 
 (deftest basic-queue-mechanics
-  (let [q (q/create 1)]
-    (is (true? (q/add q {:a 1})))
-    (is (false? (q/add q {:b 2})))
-    (is (false? (q/add q {:c 3})))
-    (is (= {:a 1} (q/fetch q 0)))
-    (is (true? (q/add q {:d 4})))
-    (is (= {:d 4} (q/fetch q 0)))
-    (is (= nil (q/fetch q 1)))
-    (is (= nil (q/fetch q 1)))
-    ))
+  (let [q (q/make-basic-queue 1)]
+    (is (true? (q/offer! q {:a 1})))
+    (is (false? (q/offer! q {:b 2})))
+    (is (false? (q/offer! q {:c 3})))
+    (is (= {:a 1} (q/poll! q 0)))
+    (is (true? (q/offer! q {:d 4})))
+    (is (= {:d 4} (q/poll! q 0)))
+    (is (nil? (q/poll! q 1)))
+    (is (nil? (q/poll! q 1)))))
+
+(def test-comparator (kits/make-comparator < :key-fn :id))
+
+(deftest priority-queue
+  (let [q (q/make-priority-queue test-comparator)]
+    (is (true? (q/offer! q {:id 2 :name :foo})))
+    (is (true? (q/offer! q {:id 1 :name :bar})))
+    (is (= :bar (:name (q/peek q))))
+    (is (= :bar (:name (q/poll! q))))
+    (is (= :foo (:name (q/poll! q))))))
