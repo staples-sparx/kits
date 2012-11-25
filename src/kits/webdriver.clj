cc62e8a47a8e838993bfec38d7479baf42a392f3
Alex Baranosky <alex@runa.com>
2012-11-25 01:04:06 -0800

    enhanced extract-html-table to work better with non-hidden tables

--- src/kits/webdriver.clj
+++ src/kits/webdriver.clj
@@ -10,11 +10,17 @@
 
 (defn extract-html-table
   "Extracts a table from a webpage, representing it as a vector of vectors."
-  [tbody-id num-cols]
+  [tbody-id num-cols & {:keys [hidden-table?]}]
   (let [table-elem (fn [row col]
                      (try
-                       (remove-td (wd/html {:css (format "table>tbody[id=%s]>tr:nth-child(%s)>td:nth-child(%s)"
-                                                         tbody-id row col)}))
+                       (let [selector {:css (format "table>tbody[id=%s]>tr:nth-child(%s)>td:nth-child(%s)" tbody-id row col)}]
+                         (if hidden-table? 
+                           ;; Webdriver needs some trickery to
+                           ;; read hidden HTML. This is not perfect
+                           ;; it won't work if there are attributes,
+                           ;; for instance, improvements are welcome
+                           (remove-td (wd/html selector))
+                           (wd/text selector)))
                        (catch Exception ex
                          :webdriver/table-exhausted)))]
     (->> (for [row (iterate inc 1)
@@ -24,3 +30,4 @@
          (partition num-cols)
          (map vec)
          vec)))
+
