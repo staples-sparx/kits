a9ac5e0aa3427050a39b83e73bacf839c093e271
Robert Levy <rplevy@runa.com>
2013-04-22 18:28:00 -0700

    update to work with lein 2

--- project.clj
+++ project.clj
@@ -1,24 +1,24 @@
-(def common-deps '[[cheshire "5.0.1"]
-                   [clj-utils "1.2.7"]
-                   [org.clojure/java.jdbc "0.2.3"]
-                   [org.clojure/tools.logging "0.2.4"]])
-
-(defproject com.runa/kits "1.4.15"
+(defproject com.runa/kits "1.4.16"
   :description "Runa base libraries"
   :plugins [[jonase/eastwood "0.0.2"]
             [lein-cloverage "1.0.2"]
             [lein-kibit "0.0.7"]
             [lein-swank "1.4.4"]
             [s3-wagon-private "1.1.2"]]
-  :repositories {"releases" {:url "s3p://runa-maven/releases/"}
-                 "snapshots" {:url "s3p://runa-maven/snapshots/"}}
-  :dependencies ~(cons '[org.clojure/clojure "1.4.0"]
-                       common-deps)
+  :repositories {"releases" {:url "s3p://runa-maven/releases/"
+                             :creds :gpg}}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "5.0.1"]
+                 [org.clojars.runa/clj-utils "1.2.8"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [org.clojure/tools.logging "0.2.4"]]
   :warn-on-reflection true
-  :dev-dependencies [[org.clojars.runa/conjure "2.0.0"]
-                     [lein-multi "1.1.0"]
-                     [slamhound "1.3.3"]]
-  :multi-deps {"1.3.0" [[org.clojure/clojure "1.3.0"]]
-               "1.4.0" [[org.clojure/clojure "1.4.0"]]
-               "1.5.0" [[org.clojure/clojure "1.5.0"]]
-               :all ~common-deps})
+  :profiles {:dev {:dependencies [[org.clojars.runa/conjure "2.0.0"]
+                                  [lein-multi "1.1.0"]
+                                  [slamhound "1.3.3"]]}
+             :1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+
+(cemerick.pomegranate.aether/register-wagon-factory!
+ "s3p" #(eval '(org.springframework.aws.maven.PrivateS3Wagon.)))
