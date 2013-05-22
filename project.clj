f1a3ee23ad8e979cec4d5a8949fafa7494ee3e60
Alex Baranosky <alexander.baranosky@gmail.com>
2013-05-22 00:45:16 -0700

    add milliseconds constants ns

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.runa/kits "1.6.2"
+(defproject org.clojars.runa/kits "1.7.0"
   :description "Runa's core utilities."
   :min-lein-version "2.0.0"
   :license {:name "MIT License"
---
+++ src/kits/milliseconds_constants.clj
@@ -0,0 +1,10 @@
+(ns kits.milliseconds-constants
+  (:refer-clojure :exclude [second]))
+
+(def ^:const second 1000)
+(def ^:const minute (* 60 second))
+(def ^:const hour (* 60 minute))
+(def ^:const day (* 24 hour))
+(def ^:const week (* 7 day))
+(def ^:const year (* 365 day))
+
