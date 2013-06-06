73ce85fdeb03ee5a3b60979c99e8180170e11906
Robert Levy <r.p.levy@gmail.com>
2013-06-06 09:54:38 -0700

    add mutable data structure option to csv utils

--- src/kits/csv.clj
+++ src/kits/csv.clj
@@ -1,9 +1,9 @@
 (ns kits.csv
-  "wrapper around clojure-csv library that turn csv in to column-name, column-value key value
-   pair that can be configured via :key-fn, :val-fn and :reader for each field "
-  (:require
-   [clojure-csv.core :as csv]))
-
+  "wrapper around clojure-csv library that turn csv in to column-name,
+   column-value key value pair that can be configured via :key-fn, :val-fn
+   and :reader for each field"
+  (:require [clojure-csv.core :as csv])
+  (:import java.util.HashMap))
 
 (def ^:dynamic *parse-opts*
   {:skip-header false
@@ -17,7 +17,8 @@
      (read-csv csv-rdr *parse-opts*))
   ([csv-rdr opts]
      (let [merged-opts (merge *parse-opts* opts)
-           {:keys [skip-header delimiter end-of-line quote-char strict]} merged-opts
+           {:keys [skip-header delimiter end-of-line
+                   quote-char strict]} merged-opts
            rows (csv/parse-csv csv-rdr
                                :delimiter delimiter
                                :end-of-line end-of-line
@@ -27,7 +28,7 @@
          (rest rows)
          rows))))
 
-(defn- csv-row->map [csv-row field-reader-opts]
+(defn- apply-field-opts-on-row [csv-row field-reader-opts]
   (let [exclude-columns (:exclude-columns field-reader-opts)]
     (reduce merge (for [i (range (count csv-row))
                         :when (or (nil? exclude-columns )
@@ -36,11 +37,29 @@
                               irow (get csv-row i)]]
                     (assoc {} (:label ifield) ((:reader ifield) irow))))))
 
-(defn csv-rows->map [csv-rows field-reader-opts]
-  (reduce merge
-          (map (fn [row]
-                 (let [amap (csv-row->map row field-reader-opts)]
-                   (assoc {} ((:key-fn field-reader-opts) amap)
-                          ((:val-fn field-reader-opts) amap)))) csv-rows)))
+(defn- hash-map->mutable [m]
+  (doto (HashMap.)
+    (#(doseq [[k v] m]
+        (.put % k v)))))
 
+(defn- apply-field-opts [csv-rows {:keys [key-fn val-fn mutable?]
+                                   :as field-reader-opts}]
+  (map (fn [row]
+         (let [row' (apply-field-opts-on-row row field-reader-opts)]
+           {(key-fn row') ((if mutable? hash-map->mutable identity)
+                           (val-fn row'))}))
+       csv-rows))
 
+(defn csv-rows->map [csv-rows {:keys [mutable?] :as field-reader-opts}]
+  (let [rows (reduce merge (apply-field-opts csv-rows field-reader-opts))]
+    (if mutable?
+      (hash-map->mutable rows)
+      rows)))
+
+(defn csv-rows->coll [csv-rows {:keys [mutable?] :as field-reader-opts}]
+  (let [rows (->> (apply-field-opts csv-rows field-reader-opts)
+                  (sort-by (comp key first)) ; key :foo in [{:foo {...}} ...]
+                  (map (comp val first)))] ; val {...} in [{:foo {...}} ...]
+    (if mutable?
+      (into-array rows)
+      rows)))
--- test/kits/test/csv.clj
+++ test/kits/test/csv.clj
@@ -3,7 +3,8 @@
   (:require
    [kits.csv :as csv]
    [clojure.java.io :as io]
-   [kits.foundation :as f]))
+   [kits.foundation :as f])
+  (:import java.util.HashMap))
 
 
 (def sample-csv (str (System/getProperty "user.dir")
@@ -19,49 +20,113 @@
    5 {:label :status :reader identity}
    6 {:label :prediction :reader identity}})
 
-(deftest parsing-csv-into-nested-maps
-  (testing "Given a csv file, generate map of maps {k-fn, the row} "
-    (is (= {1
-            {:status "1",
-             :split_point "1990",
-             :prediction "NA",
-             :split_var "most_expensive_product_in_cart",
-             :right 3,
-             :left 2,
-             :id 1},
-            2
-            {:status "1",
-             :split_point "0.5",
-             :prediction "NA",
-             :split_var "os.Windows",
-             :right 5,
-             :left 4,
-             :id 2},
-            3
-            {:status "1",
-             :split_point "1.5",
-             :prediction "NA",
-             :split_var "cart_actions",
-             :right 7,
-             :left 6,
-             :id 3},
-            4
-            {:status "1",
-             :split_point "8983.5",
-             :prediction "NA",
-             :split_var "price",
-             :right 9,
-             :left 8,
-             :id 4}}
-           (with-open [rdr (io/reader sample-csv)]
-             (csv/csv-rows->map (csv/read-csv
-                                 rdr
-                                 {:skip-header true :delimiter \space})
-                                sample-field-reader-opts))))))
+(def expected-map-result
+  {1
+   {:status "1",
+    :split_point "1990",
+    :prediction "NA",
+    :split_var "most_expensive_product_in_cart",
+    :right 3,
+    :left 2,
+    :id 1},
+   2
+   {:status "1",
+    :split_point "0.5",
+    :prediction "NA",
+    :split_var "os.Windows",
+    :right 5,
+    :left 4,
+    :id 2},
+   3
+   {:status "1",
+    :split_point "1.5",
+    :prediction "NA",
+    :split_var "cart_actions",
+    :right 7,
+    :left 6,
+    :id 3},
+   4
+   {:status "1",
+    :split_point "8983.5",
+    :prediction "NA",
+    :split_var "price",
+    :right 9,
+    :left 8,
+    :id 4}})
 
+(def expected-coll-result
+  [(get expected-map-result 1)
+   (get expected-map-result 2)
+   (get expected-map-result 3)
+   (get expected-map-result 4)])
+
+(deftest parsing-csv-into-nested-maps
+  (let [result
+        (with-open [rdr (io/reader sample-csv)]
+          (doall (csv/read-csv rdr
+                               {:skip-header true
+                                :delimiter \space})))
+        map-result
+        (csv/csv-rows->map result sample-field-reader-opts)
+
+        coll-result
+        (csv/csv-rows->coll result sample-field-reader-opts)
+
+        mutable-result
+        (with-open [rdr (io/reader sample-csv)]
+          (doall (csv/read-csv
+                  rdr
+                  {:skip-header true
+                   :delimiter \space
+                   :mutable? true})))
+
+        mutable-map-result
+        (csv/csv-rows->map mutable-result
+                           (assoc sample-field-reader-opts
+                             :mutable? true))
+
+        mutable-coll-result
+        (csv/csv-rows->coll mutable-result
+                            (assoc sample-field-reader-opts
+                              :mutable? true))]
+
+    (testing "Given a csv, generate map of maps {k-fn, the row}"
+      (is (= expected-map-result
+             map-result))
+      (is (= clojure.lang.PersistentArrayMap
+             (type map-result)))
+      (is (= clojure.lang.PersistentArrayMap
+             (type (get map-result 1)))))
+
+    (testing "Given a csv, generate a coll of maps {k-fn, the row}"
+      (is (= expected-coll-result
+             coll-result))
+      (is (= clojure.lang.PersistentVector
+             (type (vec coll-result))))
+      (is (= clojure.lang.PersistentArrayMap
+             (type (first coll-result)))))
+
+    (testing
+        "Given a csv, generate mutable map of mutable maps {k-fn, the row}"
+      (is (= (get expected-map-result 1)
+             (first (.values mutable-map-result))))
+      (is (= java.util.HashMap
+             (type (first (.values mutable-map-result)))))
+      (is (= java.util.HashMap
+             (type mutable-map-result))))
+
+    (testing
+        "Given a csv, generate mutable array of mutable maps {k-fn, the row}"
+      (is (= (first expected-coll-result)
+             (first mutable-coll-result)))
+      (is (= java.util.HashMap
+             (type (first mutable-coll-result))))
+      (is (= "class [Ljava.util.HashMap;"
+             (str (type mutable-coll-result)))))))
 
 (deftest parsing-csv-into-nested-maps-with-columns-excluded
-  (testing "Given a csv file, generate map of maps {k-fn, the row} excludes columns specified"
+  (testing
+      "Given a csv, generate map of maps {k-fn, the row} excludes columns"
     (is (= {1
             {:status "1",
              :split_point "1990",
