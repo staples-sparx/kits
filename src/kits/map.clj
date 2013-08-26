622260d0a3337fc5ad95967de0fbb6ee7cd440ce
Alex Baranosky <alexander.baranosky@gmail.com>
2013-08-26 12:27:33 -0700

    removed 'apply' from deep-merge-with

--- src/kits/map.clj
+++ src/kits/map.clj
@@ -137,22 +137,20 @@
   ([m path & more-paths]
      (apply dissoc-in (dissoc-in m path) more-paths)))
 
-;; Alex - TODO - 6/23/12 (M/D/Y) - copied from contrib.
-;; Add some unit tests
 
+;; Add some unit tests
 (defn deep-merge-with
   "Like merge-with, but merges maps recursively, applying the given fn
   only when there's a non-map at a particular level.
 
-  (deep-merge-with + {:a {:b {:c 1 :d {:x 1 :y 2}} :e 3} :f 4}
-                     {:a {:b {:c 2 :d {:z 9} :z 3} :e 100}})
+  (deep-merge-with + [{:a {:b {:c 1 :d {:x 1 :y 2}} :e 3} :f 4}
+                      {:a {:b {:c 2 :d {:z 9} :z 3} :e 100}}])
   -> {:a {:b {:z 3, :c 3, :d {:z 9, :x 1, :y 2}}, :e 103}, :f 4}"
-  [f & maps]
-  (apply
-   (fn m [& maps]
+  [f maps]
+  ((fn m [maps]
      (if (every? map? maps)
-       (apply merge-with m maps)
-       (apply f maps)))
+       (merge-with m maps)
+       (f maps)))
    maps))
 
 (defn invert-map
