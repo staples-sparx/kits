503b23e18369a178dc5ee1c4fe4c28d7e77bd95a
Alan Thompson <alan@stapleslabs.com>
2014-04-24 15:24:26 -0700

    Cleaned up haversine formula. Added tests.

--- src/kits/geometry.clj
+++ src/kits/geometry.clj
@@ -6,28 +6,49 @@
 
 (def ^:const max-error-ratio 1e-4)
 
-(defn haversine-distance-km [from-lat from-lon to-lat to-lon]
-  (let [sin-dlat (Math/sin (/ (Math/toRadians (- to-lat from-lat)) 2))
-        sin-dlon (Math/sin (/ (Math/toRadians (- to-lon from-lon)) 2))
-        a (+ (Math/pow sin-dlat 2)
-             (* (Math/pow sin-dlon 2)
-                (Math/cos (Math/toRadians from-lat))
-                (Math/cos (Math/toRadians to-lat))))]
-    (* earth-radius-km 
-       (* 2 (Math/atan2 (Math/sqrt a) (Math/sqrt (- 1 a)))
-    ))))
+(defn square-dbl [^:double dbl-val]
+  (* dbl-val dbl-val))
+
+(defn haversine 
+  "Returns the haversine function of an angle in radians."
+  [^:double theta]
+  (square-dbl (Math/sin (/ theta 2))) )
+
+(defn haversine-great-circle-angle
+  "Returns the angle in radians between to lat/lon points in radians."
+  [^:double lat1 ^:double lon1  
+   ^:double lat2 ^:double lon2 ]
+  (* 2 (Math/asin 
+         (Math/sqrt
+           (+ (haversine (- lat1 lat2))
+              (* (Math/cos lat1) (Math/cos lat2) 
+                 (haversine (- lon1 lon2)) ))))))
+
+(defn haversine-great-circle-km
+  "Returns the distance in kilometers between to lat/lon points in degrees"
+  [^:double lat1-deg ^:double lon1-deg  
+   ^:double lat2-deg ^:double lon2-deg]
+  (let [lat1-rad (Math/toRadians lat1-deg)
+        lon1-rad (Math/toRadians lon1-deg)
+        lat2-rad (Math/toRadians lat2-deg)
+        lon2-rad (Math/toRadians lon2-deg)
+
+        gc-angle-rad (haversine-great-circle-angle  lat1-rad lon1-rad 
+                                                    lat2-rad lon2-rad )
+        gc-dist-km (* gc-angle-rad earth-radius-km) ]
+      gc-dist-km ))
 
 (defn -main []
-  (let [dist-kits (haversine-distance-km  33.088678 -117.242227
-                                          37.561820 -122.324625)
+  (let [dist-kits (haversine-great-circle-km  33.088678 -117.242227
+                                              37.561820 -122.324625)
         dist-ref  677.9969
         delta     (Math/abs (- dist-ref dist-kits)) 
         ratio     (/ delta earth-radius-km)
   ]
     (assert (< ratio max-error-ratio))
   )
-  (let [dist-kits (haversine-distance-km  33.088678 -117.242227
-                                          40.689215, -74.044627)
+  (let [dist-kits (haversine-great-circle-km  33.088678 -117.242227
+                                              40.689215, -74.044627)
         dist-ref  3892.9607
         delta     (Math/abs (- dist-ref dist-kits)) 
         ratio     (/ delta earth-radius-km)
