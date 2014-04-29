44b75504df9cccdacc73f5f5e8748bab5df2dd37
Alan Thompson <thompson2526@gmail.com>
2014-04-29 12:07:56 -0700

    Added kits.geometry/simple-dist-km, plus tests & cleanup

--- src/kits/geometry.clj
+++ src/kits/geometry.clj
@@ -7,10 +7,18 @@
   "Earth radius (volumetric mean - NASA)" 
   6371 )
 
+(def ^:const deg->km
+  "Distance in km for each degree on earth's surface (spherical model)" 
+  (-> earth-radius-km 
+      (* 2 Math/PI)
+      (/ 360)
+      (double) ))
+
 (defn square-dbl 
-  "Square a double value." 
-  [^:double dbl-val]
-  (* dbl-val dbl-val))
+  "Returns the square of a value as a double" 
+  [val]
+  (let [dbl-val (double val)]
+    (* dbl-val dbl-val) ))
 
 (defn haversine 
   "Returns the haversine function of an angle in radians."
@@ -27,17 +35,32 @@
               (* (Math/cos lat1) (Math/cos lat2) 
                  (haversine (- lon1 lon2)) ))))))
 
-(defn great-circle-km
-  "Returns the distance in kilometers between to lat/lon points in degrees (haversine algorithm)."
-  [^:double lat1-deg ^:double lon1-deg  
-   ^:double lat2-deg ^:double lon2-deg]
-  (let [lat1-rad (Math/toRadians lat1-deg)
-        lon1-rad (Math/toRadians lon1-deg)
-        lat2-rad (Math/toRadians lat2-deg)
-        lon2-rad (Math/toRadians lon2-deg)
+(defn great-circle-dist-km
+  "Returns the distance in kilometers between to lat/lon points in degrees 
+  (haversine algorithm)."
+  [lat1-deg lon1-deg  
+   lat2-deg lon2-deg]
+  (let [lat1-rad (Math/toRadians (double lat1-deg))
+        lon1-rad (Math/toRadians (double lon1-deg))
+        lat2-rad (Math/toRadians (double lat2-deg))
+        lon2-rad (Math/toRadians (double lon2-deg))
 
         gc-angle-rad (great-circle-radians-haversine  lat1-rad lon1-rad 
                                                       lat2-rad lon2-rad )
         gc-dist-km (* gc-angle-rad earth-radius-km) ]
       gc-dist-km ))
+
+(defn simple-dist-km
+  "Returns the approximate (flat-earth) distance in kilometers between to lat/lon points
+  in degrees."
+  [lat1-deg lon1-deg  
+   lat2-deg lon2-deg]
+  (let [mean-lat         (Math/toRadians (/ (double (+ lat1-deg lat2-deg)) 2))
+        lat-cos          (Math/cos mean-lat)
+        lat-dist-deg     (double            (- lat1-deg lat2-deg))
+        lon-dist-deg     (double (* lat-cos (- lon1-deg lon2-deg)))
+        dist-deg         (Math/sqrt (+ (square-dbl lat-dist-deg) 
+                                       (square-dbl lon-dist-deg) ))
+        dist-km          (* dist-deg deg->km) ]
+    dist-km ))
 
