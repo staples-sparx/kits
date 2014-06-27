30153f1dcd6158905043344ba3c2a8c006dd222f
Alex Baranosky <alexander.baranosky@gmail.com>
2014-06-27 01:15:40 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.runa/kits "1.17.15-SNAPSHOT"
+(defproject org.clojars.runa/kits "1.18.1-SNAPSHOT"
   :description "Runa's core utilities."
   :min-lein-version "2.0.0"
   :license {:name "MIT License"
