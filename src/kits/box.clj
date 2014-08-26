a2d1634eba5b0692cfbc70f240cb2623e0b4614b
Alex Baranosky <alexander.baranosky@gmail.com>
2014-08-26 00:43:31 -0700

    make boxes support more interfaces

--- src/kits/box.clj
+++ src/kits/box.clj
@@ -2,5 +2,13 @@
   "It's a box. You can deref it.")
 
 (defn box [val]
-  (reify clojure.lang.IDeref
-    (deref [_] val)))
+  (reify
+    clojure.lang.IDeref
+    (deref [_]
+      val)
+    clojure.lang.IBlockingDeref
+    (deref [_ _timeout-ms _timeout-val]
+      val)
+    clojure.lang.IPending
+    (isRealized [_]
+      true)))
