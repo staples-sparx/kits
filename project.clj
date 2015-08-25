7c746f0bd4b6f6c7a5b35bf21b1bae03134a8489
Ryan Sundberg <ryan.sundberg@gmail.com>
2015-08-25 09:06:15 -0700

    Upgrade to clojure 1.7, add core.async, clj-http

--- project.clj
+++ project.clj
@@ -7,8 +7,10 @@
   :url "https://github.com/staples-sparx/kits"
   :plugins [[jonase/eastwood "0.0.2"]
             [lein-kibit "0.0.7"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [cheshire "5.3.1"]
+                 [clj-http "2.0.0"]
                  [clout "1.1.0"]
                  [org.hdrhistogram/HdrHistogram "1.2.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]
@@ -20,8 +22,10 @@
   :global-vars {*warn-on-reflection* false}
   :profiles {:dev {:dependencies [[slamhound "1.3.3"]
                                   [cider/cider-nrepl "0.8.2"]]}
-             :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5.0 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
   :aliases {"run-tests" ["with-profile" "1.4.0:1.5.0" "test"]
